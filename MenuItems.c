a19991218d8b09fd07aa0585929f3dd4c46e6508
da9876 <saadyaya5@gmail.com>
2021-01-13 16:56:41 +0500

    Menu item

--- MenuItems.c
+++ MenuItems.c
@@ -1,8 +1,22 @@
-
+#include<stdio.h>  
+#include <string.h>
 struct Item{
-	int itemId;
-	char[20] itemName;
+	int Id;
+	char[20] code;
+	char[20] name;
 	int price;
+	char[20] parentCode;
+	int isparent;
+	
 	
 };
+
+struct Item items[10]; 
+//	id:11,
+//    code:'PO7',
+//    name:'fAST fOOD',
+//    price:'',
+//    desc:'',
+//    parentCode:'',
+//    isParent:1
 
